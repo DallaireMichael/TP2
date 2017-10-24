7000203b12465bee83b274cbec29d08438e78bbe
Michaël Dallaire <michael.dallaire.1@etsmtl.net>
2017-10-24 16:09:12 -0400

    modif Identification
    Docteur non fini

--- src/Docteur.java
+++ src/Docteur.java
@@ -1,6 +1,90 @@
 import java.io.Serializable;
 
-
+/**
+ * Décrit une personne ayant comme titre de Docteur
+ * Elle permet d'accéder à son identification et son 
+ * départememnt
+ * @author Michaël Dallaire
+ * @version 1.0
+ */
 public class Docteur implements Serializable{
 	
+	/*
+	 * ATTRIBUTS
+	 */
+	
+	//L'instance d'identification
+	private Identification id;
+	
+	//Le numéro du département
+	private int departement;
+	
+	/*
+	 * CONSTRUCTEURS
+	 */
+	
+	/**
+	 * Constructeur par défaut
+	 */
+	public Docteur(){
+		
+		id = new Identification();
+		departement = 0;
+		
+	}
+	
+	
+	/**
+	 * Constructeur par copie d'attributs
+	 * @param id
+	 * @param departement
+	 */
+	public Docteur(Identification id, int departement){
+		
+		this.id = id.clone();
+		this.departement = departement;
+		
+	}
+	
+	/**
+	 * Constructeur par copie d'objet
+	 * @param doc
+	 */
+	public Docteur(Docteur doc){
+		
+		id = doc.id.clone();
+		departement = doc.departement;
+	}
+	
+	/*
+	 * MUTATEURS
+	 */
+	
+	/**
+	 * Mute l'identification du docteur 
+	 * @param id
+	 */
+	public void setId(Identification id){
+		this.id = id.clone();
+	}
+	
+	/**
+	 * Mute departement
+	 * @param departement
+	 */
+	public void setDepartement(int departement){
+		this.departement = departement;
+	}
+	
+	/*
+	 * ACCESSEURS 
+	 */
+	
+	/**
+	 * Retourne son identification
+	 * @return id
+	 */
+	public Identification getId(){
+		return id;
+	}
 }
