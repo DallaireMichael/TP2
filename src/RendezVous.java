fc7c7b839f00214d12d235d6ca3c220ceab32c69
Niko Girardelli <niko.girardelli.1@etsmtl.net>
2017-10-29 17:20:08 -0400

    RendezVous corrigé le constructeur.

--- src/RendezVous.java
+++ src/RendezVous.java
@@ -41,7 +41,7 @@ public class RendezVous implements Serializable {
 		}
 		
 		this.patient = patient.clone();
-		this.docteur = docteur.clone();
+		this.docteur = docteur.clone(docteur);
 		this.infirmier = infirmier.clone();
 		
 	}
